9f6021e79d4bbe93c74a09dee143dca1a6cb9fe6
renact <renact@protonmail.com>
2018-01-11 23:50:28 +0100

    Video preview component - init

--- src/app/stream/stream.module.ts
+++ src/app/stream/stream.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { PreviewComponent } from './preview/preview.component'
 
 import { routing } from './stream.routing';
 
 @NgModule({
     declarations: [
         OverviewComponent,
+        PreviewComponent,
     ],
     imports: [ 
         routing,
