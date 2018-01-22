9a7731f5c9ffed4fc1ddae0aed1c94d1299711dd
renact <renact@protonmail.com>
2018-01-22 22:41:56 +0100

    added js files

--- src/app/stream/stream.module.ts
+++ src/app/stream/stream.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
 import { PreviewComponent } from './preview/preview.component'
+import { StreamComponent } from 'app/stream/shared/stream';
 
 import { routing } from './stream.routing';
 
@@ -9,6 +10,7 @@ import { routing } from './stream.routing';
     declarations: [
         OverviewComponent,
         PreviewComponent,
+        StreamComponent,
     ],
     imports: [ 
         routing,
